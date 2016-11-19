4ee0c4df11b5c0ed2a4bf332388b757d0c5395f6
antranthien <an.t.thien@hiof.no>
2016-11-19 12:47:37 +0100

    Initial Commit

---
+++ Chat With Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat With Firebase
+//
+//  Created by admin on 19/11/16.
+//  Copyright © 2016 An Tran Thien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Chat With FirebaseTests/Chat_With_FirebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chat_With_FirebaseTests.swift
+//  Chat With FirebaseTests
+//
+//  Created by admin on 19/11/16.
+//  Copyright © 2016 An Tran Thien. All rights reserved.
+//
+
+import XCTest
+@testable import Chat_With_Firebase
+
+class Chat_With_FirebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
